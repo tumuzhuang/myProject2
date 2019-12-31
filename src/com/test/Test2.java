0db1145a8256768b543771e3cfb1ec381b2e7a87
lule <1721753977@qq.com>
2019-12-31 15:36:44 +0800

    commit Test2.java begin...

--- src/com/test/Test2.java
+++ src/com/test/Test2.java
@@ -4,6 +4,7 @@ import com.entity.User;
 
 public class Test2 {
     public static void main(String[] args) {
+        System.out.println("begin....");
         System.out.println("nohello");
         System.out.println("helloxian");
         System.out.println("hellochina");
