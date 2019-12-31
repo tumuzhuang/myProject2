c1eeec5c55a84ca81bca66dee05138931ed5816e
lule <1721753977@qq.com>
2019-12-31 14:52:19 +0800

    anther people commit hello niubi

--- src/com/test/Test2.java
+++ src/com/test/Test2.java
@@ -6,6 +6,7 @@ public class Test2 {
     public static void main(String[] args) {
         User user=new User("asan2","asan come in yindu");
         System.out.println(user);
+        System.out.println("niubi");
     }
 
 }
